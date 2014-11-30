d0c4e1e56206e0740fbb59e4fe8b1a0ea74dd36a
Ian Ooi <ionaic@gmail.com>
2014-11-30 03:28:19 -0500

    Constraints on rotations work for axis-angle rotations! (Should also work for euler angle rotations but hasn't been tested in a while since I'm no longer using it).

--- auto-animation/Assets/PhysicalJoint.cs
+++ auto-animation/Assets/PhysicalJoint.cs
@@ -28,35 +28,58 @@ public class PhysicalJoint : MonoBehaviour {
     
     public bool CanRoll() {
         return canRoll && 
-            (Mathf.Approximately(minRollAngle, jointTransform.eulerAngles.z) || 
-            Mathf.Approximately(maxRollAngle, jointTransform.eulerAngles.z));
+            (!Mathf.Approximately(minRollAngle, jointTransform.eulerAngles.z) || 
+            !Mathf.Approximately(maxRollAngle, jointTransform.eulerAngles.z));
     }
     public bool CanPitch() {
         return canPitch && 
-            (Mathf.Approximately(minPitchAngle, jointTransform.eulerAngles.x) || 
-            Mathf.Approximately(maxPitchAngle, jointTransform.eulerAngles.x));
+            (!Mathf.Approximately(minPitchAngle, jointTransform.eulerAngles.x) || 
+            !Mathf.Approximately(maxPitchAngle, jointTransform.eulerAngles.x));
     }
     public bool CanYaw() {
         return canYaw && 
-            (Mathf.Approximately(minYawAngle, jointTransform.eulerAngles.y) || 
-            Mathf.Approximately(maxYawAngle, jointTransform.eulerAngles.y));
+            (!Mathf.Approximately(minYawAngle, jointTransform.eulerAngles.y) || 
+            !Mathf.Approximately(maxYawAngle, jointTransform.eulerAngles.y));
     }
 
     public void Rotate(Vector3 eulerAngles) {
         Rotate(eulerAngles.x, eulerAngles.y, eulerAngles.z);
     }
     public void Rotate(float pitch, float yaw, float roll) {
-        float clampedX = Mathf.Clamp(pitch, minPitchAngle, maxPitchAngle);
-        float clampedY = Mathf.Clamp(yaw, minPitchAngle, maxPitchAngle);
-        float clampedZ = Mathf.Clamp(roll, minPitchAngle, maxPitchAngle);
+        float clampedX = CanPitch() ? 
+            Mathf.Clamp(pitch, minPitchAngle, maxPitchAngle) 
+            : 0.0f;
+        float clampedY = CanYaw() ?
+            Mathf.Clamp(yaw, minYawAngle, maxYawAngle)
+            : 0.0f;
+        float clampedZ = CanRoll() ? 
+            Mathf.Clamp(roll, minRollAngle, maxRollAngle)
+            : 0.0f;
         jointTransform.Rotate(clampedX, clampedY, clampedZ);
     }
     public void Rotate(Vector3 point, Vector3 axis, float angle) {
         axis.Normalize();
-        jointTransform.RotateAround(point, axis, angle);
+        jointTransform.RotateAround(point, ConstrainAxis(axis), angle);
+        //ReClamp();
     }
     public void ReClamp() {
+        Vector3 angles = jointTransform.eulerAngles;
+        angles.x = Mathf.Clamp(angles.x, minPitchAngle, maxPitchAngle);
+        angles.y = Mathf.Clamp(angles.y, minYawAngle, maxYawAngle);
+        angles.z = Mathf.Clamp(angles.z, minRollAngle, maxRollAngle);
+        jointTransform.eulerAngles = angles;
+    }
 
+    public Vector3 ConstrainAxis(Vector3 axis) {
+        Vector3 tmp = axis;
+        tmp.x = CanPitch() ? tmp.x : 0.0f;
+        tmp.y = CanYaw() ? tmp.y : 0.0f;
+        tmp.z = CanRoll() ? tmp.z : 0.0f;
+        return tmp;
+    }
+
+    public Vector3 ConstrainedEuler(Vector3 eulerAngles) {
+        return eulerAngles;
     }
 
     void Start() {
