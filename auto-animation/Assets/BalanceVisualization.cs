0a1460dad925b9692f2bb25904908cde5314acff
Ian Ooi <ionaic@gmail.com>
2014-12-15 18:27:33 -0500

    Adding screencaps for use in papers

--- auto-animation/Assets/BalanceVisualization.cs
+++ auto-animation/Assets/BalanceVisualization.cs
@@ -15,8 +15,26 @@ public class BalanceVisualization : MonoBehaviour {
                             0, 2, 3, 
                             2, 1, 0, // include back faces as well
                             3, 2, 0 };
+    private Vector3[] norms = { 
+         Vector3.up,
+         Vector3.up,
+         Vector3.up,
+         Vector3.up
+    };
+
+    private Vector2[] uv = {
+        new Vector2(0, 0),
+        new Vector2(1, 0),
+        new Vector2(0, 1),
+        new Vector2(1, 1)
+    };
+
 	// Use this for initialization
 	void Start () {
+        CreateMesh();
+	}
+
+    void CreateMesh() {
         supportingPlaneMesh = new Mesh();
         meshFilter = GetComponent<MeshFilter>();
         meshRenderer = GetComponent<MeshRenderer>();
@@ -24,15 +42,19 @@ public class BalanceVisualization : MonoBehaviour {
 
         meshFilter.mesh = supportingPlaneMesh;
         controller.UpdateSupportingPoly();
+        UpdateMesh();
         supportingPlaneMesh.triangles = faces;
+        supportingPlaneMesh.normals = norms;
+        supportingPlaneMesh.uv = uv;
         UpdateMesh();
         meshFilter.sharedMesh = supportingPlaneMesh;
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
         //Debug.Log("Supporting Poly: " + controller.supportingPoly.Length);
         UpdateMesh();
+        //CreateMesh();
 	}
     
     void UpdateMesh() {
